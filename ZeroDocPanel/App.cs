d0c61fa09be9cddc414593b8781ca5e38b62aadf
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-09-11 10:38:52 +0200

    initial commit from original blog post

---
+++ ZeroDocPanel/App.cs
@@ -0,0 +1,64 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace ZeroDocPanel
+{
+  public class Availability 
+    : IExternalCommandAvailability
+  {
+    public bool IsCommandAvailable(
+      UIApplication a, 
+      CategorySet b )
+    {
+      return true;
+    }
+  }
+
+  [Transaction( TransactionMode.Manual )]
+  [Regeneration( RegenerationOption.Manual )]
+  class App : IExternalApplication
+  {
+    public const string Caption = "Zero Doc Panel";
+    public const string Caption2 = "Zero Doc Button";
+
+    static string _path 
+      = System.Reflection.Assembly
+        .GetExecutingAssembly().Location;
+
+    public Result OnStartup( 
+      UIControlledApplication a )
+    {
+      PushButtonData d = new PushButtonData( 
+        Caption2, Caption2, _path, 
+        "ZeroDocPanel.Command" );
+
+      d.AvailabilityClassName 
+        = "ZeroDocPanel.Availability";
+
+      RibbonPanel p = a.CreateRibbonPanel( Caption );
+
+      PushButton b = p.AddItem( d ) as PushButton;
+
+      b.ToolTip 
+        = "This is the zero doc panel button tooltip";
+
+      //b.AvailabilityClassName 
+      //  = "ZeroDocPanel.Availability";
+
+      return Result.Succeeded;
+    }
+
+    public Result OnShutdown( 
+      UIControlledApplication a )
+    {
+      return Result.Succeeded;
+    }
+  }
+}
---
+++ ZeroDocPanel/Command.cs
@@ -0,0 +1,25 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+#endregion
+
+namespace ZeroDocPanel
+{
+  [Transaction( TransactionMode.Manual )]
+  [Regeneration( RegenerationOption.Manual )]
+  public class Command : IExternalCommand
+  {
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      TaskDialog.Show( App.Caption, App.Caption2 );
+      return Result.Succeeded;
+    }
+  }
+}
