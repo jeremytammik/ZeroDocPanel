e7ab078bc17b527284715271ffd65731074af709
Jeremy Tammik <jeremy.tammik@autodesk.com>
2018-09-11 11:14:38 +0200

    added assembly description

--- ZeroDocPanel/Properties/AssemblyInfo.cs
+++ ZeroDocPanel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Revit Add-In Title" )]
-[assembly: AssemblyDescription( "Revit Add-In Description" )]
+[assembly: AssemblyTitle( "ZeroDocPanel" )]
+[assembly: AssemblyDescription( "Revit C# .NET add-in IExternalCommandAvailability demo" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "Autodesk Inc." )]
-[assembly: AssemblyProduct( "Revit Add-In Template Application" )]
+[assembly: AssemblyCompany( "Autodesk Inc. and The Building Coder" )]
+[assembly: AssemblyProduct( "ZeroDocPanel" )]
 [assembly: AssemblyCopyright( "Copyright 2010-2018 © Jeremy Tammik Autodesk Inc." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -38,6 +38,8 @@ using System.Runtime.InteropServices;
 // 2011-02-16 1.0.0.0 initial blog post and release for Revit 2012
 // 2018-09-11 problem described with Revit 2019 in https://forums.autodesk.com/t5/revit-api-forum/iscommandavailable-causes-nullref-exception/m-p/8259249
 // 2018-09-11 2019.0.0.0 flat migration to Revit 2019
-
+// 2018-09-11 2019.0.0.0 added VendorId node
+// 2018-09-11 2019.0.0.0 added assembly description
+//
 [assembly: AssemblyVersion( "2019.0.0.0" )]
 [assembly: AssemblyFileVersion( "2019.0.0.0" )]
